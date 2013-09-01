61c3b19273abd467feae813d1a32e3dfca070661
root <root@sheevaplug-debian.(none)>
2013-09-01 09:03:00 -0400

    updated pkg dir

---
+++ pkg/makepackage.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+ROOT_DIR=~/dataware.energy
+PKG_DIR=$ROOT_DIR/pkg/package_files
+cd $ROOT_DIR/src
+rm -rf deb_dist
+python setup.py --command-packages=stdeb.command sdist_dsc
+cd deb_dist/dataware-resource-0.1/debian
+cp $PKG_DIR/control ./
+cp $PKG_DIR/config ./
+cp $PKG_DIR/postinst ./
+cp $PKG_DIR/rules ./
+cp $PKG_DIR/dirs ./
+cd ..
+cp $PKG_DIR/mysql.sql ./
+dpkg-buildpackage -rfakeroot -uc -us
+cd debian/dataware-resource
+mkdir -p var/dataware/
+mkdir -p etc/dataware
+mkdir -p var/log/dataware
+chmod -R 777 var/log/dataware
+mv ../../dataware/static ./var/dataware
+mv ../../dataware/views  ./var/dataware
+mv ../../dataware/config.cfg ./etc/dataware/sample_config.cfg
+cd ..
+dpkg --build dataware-resource dataware-resource.deb
+cp dataware-resource.deb $ROOT_DIR 
